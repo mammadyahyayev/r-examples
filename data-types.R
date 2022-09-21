614aea9c14487364f4c91d7f6067c9ba75f1c69e
MamedYahyayev <yahyayev2001@gmail.com>
2022-09-21 14:14:01 +0400

    data types

---
+++ data-types.R
@@ -0,0 +1,25 @@
+numeric_value = 123
+numeric_value_2 = 34.56
+
+numeric_value
+numeric_value_2
+
+character_value = "Hello World"
+character_value_2 = 'Hello World with single quote'
+
+character_value
+character_value_2
+
+
+logical_value_TRUE = TRUE
+logical_value_T = T
+
+
+logical_value_FALSE = FALSE
+logical_value_F = F
+
+
+logical_value_FALSE
+logical_value_F
+logical_value_TRUE
+logical_value_T
