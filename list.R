6414825d52e29c9b93cf70e42f52c6db4281bc65
MamedYahyayev <yahyayev2001@gmail.com>
2022-09-21 14:43:09 +0400

    lists

---
+++ list.R
@@ -0,0 +1,16 @@
+vect1 <- c(1, 2, 3, 4)
+vect2 <- c(13, 5, 7, 34)
+
+mix <- list(vect1, vect2)
+mix
+
+new_mix <- list(mix, 20, 25, 44, TRUE)
+new_mix
+
+mean <- list(1, 1, 2, 2, 2, 3, 3, 31, 31, 32, 32, 32, 33, 33, mean)
+
+class(list) # function, because at the end of the list there is a function called mean
+
+median(c(1, 1, 2, 2, 2, 3, 3, 31, 31, 32, 32, 32, 33, 33))
+mean(c(1, 1, 2, 2, 2, 3, 3, 31, 31, 32, 32, 32, 33, 33))
+
