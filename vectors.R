2f8fd7e3ad1463dfc93d336dd1e6e8da6e87e52b
MamedYahyayev <yahyayev2001@gmail.com>
2022-09-21 14:29:09 +0400

    vectors

---
+++ vectors.R
@@ -0,0 +1,25 @@
+gender <- c("male", "female")
+scores <- c(123, 345, 234, 123, 345, 234, 678, 954, 347)
+status <- c(TRUE, FALSE, T, F)
+mix <- c(TRUE, FALSE, 2344, 23, 12, "hey", 'aa') # if we mix data types, they will be converted into character
+
+vect1 <- c(1, 2, 3, 4)
+vect2 <- c(13, 5, 7, 34)
+
+result <- vect1 + vect2 # it will sum of each value with its appropriate
+
+mix_vect <- c(vect1, vect2)
+
+gender
+scores
+status
+mix
+result
+mix_vect
+
+class(gender) # character
+class(scores) # numeric
+class(status) # logical
+class(mix) # character
+class(vect1) # numeric
+class(mix_vect) # numeric
