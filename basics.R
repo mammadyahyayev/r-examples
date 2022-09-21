8a6e23425ef55178c62dbc4ab66e86c04ce2bdd9
MamedYahyayev <yahyayev2001@gmail.com>
2022-09-21 14:13:51 +0400

    basics in R

---
+++ basics.R
@@ -0,0 +1,16 @@
+1 + 2
+
+num = 4 + 5
+num2 <- 45 + 44 # assignment operator
+
+num # this will print the result on the console
+
+multiply = num * 10
+
+multiply
+num2
+
+# R is case sensitive
+case = 34
+Case = 56
+cAse = 33
